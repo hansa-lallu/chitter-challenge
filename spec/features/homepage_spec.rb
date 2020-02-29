112767e21a1dff847bd08343bacb48395c28a453
hansa-lallu <hansalallu@gmail.com>
2020-02-29 16:44:01 +0000

    fixed set up, rspec now testing test database

---
+++ spec/features/homepage_spec.rb
@@ -0,0 +1,6 @@
+feature 'view home page' do
+  scenario 'Basic greeting on home page' do
+    visit ('/')
+    expect(page).to have_content('Welcome to Chitter!')
+  end
+end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,11 @@
 require 'simplecov'
 require 'simplecov-console'
+require 'capybara'
+require 'rspec'
+require "capybara/rspec"
+require "sinatra/activerecord"
+require_relative "../app/app"
+
 
 SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter.new([
   SimpleCov::Formatter::Console,
@@ -14,4 +20,36 @@ RSpec.configure do |config|
     puts "\e[33mHave you considered running rubocop? It will help you improve your code!\e[0m"
     puts "\e[33mTry it now! Just run: rubocop\e[0m"
   end
+end
+
+# ENV["ENVIRONMENT"] = "test"
+ENV['RACK_ENV'] = 'test'
+ENV['ENVIRONMENT'] = 'test'
+
+Capybara.app = Chitter
+
+Capybara.default_driver = :selenium
+Capybara.server = :webrick
+
+DB_ENV ||='test'
+connection_details =YAML::load(File.open('./config/database.yml'))
+ActiveRecord::Base.establish_connection(connection_details[DB_ENV])
+
+RSpec.configure do |config|
+  config.before(:each) do
+    ActiveRecord::Base.connection.execute('DELETE FROM users;')
+    ActiveRecord::Base.connection.execute('DELETE FROM messages;')
+  end
+  # rspec-expectations config goes here. You can use an alternate
+  # assertion/expectation library such as wrong or the stdlib/minitest
+  # assertions if you prefer.
+  config.expect_with :rspec do |expectations|
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+
+  config.shared_context_metadata_behavior = :apply_to_host_groups
 end
